dffe992c11e4f3a0a6cc133ade58236794c68850
Shawnade <deshawnagalloway@yahoo.com>
2025-04-12 17:36:40 -0400

    completed levelup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
+// Later on, we'll import button components here
 
+import Button1 from './components/example-1-normal-css/Button';
+import Button2 from './components/example-2-inline-css/Button';
+import Button3 from './components/example-3-styled-components/Button';
+import Button4 from './components/example-4-css-modules/Button'
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      {
+        // Add all example components:
+        <>
+          // Add all example components:
+          <Button1 buttonText="Example 1" />
+          <Button2 />
+          <Button3 />
+          <Button4 />
+          </>
+        //...
+      }
+    </div>
+  );
+};
 
-export default App
+export default App;
